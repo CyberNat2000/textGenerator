1661dc8b9916e08b0b56325faf4f2750c6d1573d
CyberNat2000 <77163754+CyberNat2000@users.noreply.github.com>
2023-01-16 14:00:35 +0100

    Create nawigacja.js

---
+++ nawigacja.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName("nav").innerHTML=
+ `<a href=index>Strona Główna</a>
+<a href="TextWspak">Tekst Wspak</a>
+<a href="GeneratorCzcionek">Generator Czcionek</a>
+<a href="UpsideDownText">Tekst do góry nogami</a>`;
