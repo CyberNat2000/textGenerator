e629f2be5183f39fff44ba01c7a7546b750607a7
CyberNat2000 <77163754+CyberNat2000@users.noreply.github.com>
2023-01-16 14:37:07 +0100

    Update nawigacja.js

--- nawigacja.js
+++ nawigacja.js
@@ -1,1 +1,1 @@
-document.getElementsById("nav").innerHTML=`<a href=index>Strona Główna</a><a href="TextWspak">Tekst Wspak</a><a href="GeneratorCzcionek">Generator Czcionek</a><a href="UpsideDownText">Tekst do góry nogami</a>`;
+document.getElementById("nav").innerHTML=`<a href=index>Strona Główna</a><a href="TextWspak">Tekst Wspak</a><a href="GeneratorCzcionek">Generator Czcionek</a><a href="UpsideDownText">Tekst do góry nogami</a>`;
