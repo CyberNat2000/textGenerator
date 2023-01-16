0e381007e975bfc2e9a21149eb2ab416f55e1b3c
CyberNat2000 <77163754+CyberNat2000@users.noreply.github.com>
2023-01-16 14:09:58 +0100

    Update nawigacja.js

--- nawigacja.js
+++ nawigacja.js
@@ -1,4 +1,4 @@
-document.getElementsByName("nav").innerHTML=
+document.getElementsById("nav").innerHTML=
  `<a href=index>Strona Główna</a>
 <a href="TextWspak">Tekst Wspak</a>
 <a href="GeneratorCzcionek">Generator Czcionek</a>
